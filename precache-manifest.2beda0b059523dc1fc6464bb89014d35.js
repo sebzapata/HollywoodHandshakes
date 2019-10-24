f9ccc154f5cb97052ce2de705d1a0b6ff6d3b8bd
Seb Zapata <sebzapata@hotmail.co.uk>
2019-10-24 13:30:48 +0100

    Updates

---
+++ precache-manifest.2beda0b059523dc1fc6464bb89014d35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee15312de5de7ad7af22e9f8e6b5f0b",
+    "url": "/HollywoodHandshakes/index.html"
+  },
+  {
+    "revision": "f4c3b4f4f9fc8bbb6e86",
+    "url": "/HollywoodHandshakes/static/css/main.d4f93819.chunk.css"
+  },
+  {
+    "revision": "ee7224e8ecb2930256dc",
+    "url": "/HollywoodHandshakes/static/js/2.8ba79f5c.chunk.js"
+  },
+  {
+    "revision": "f4c3b4f4f9fc8bbb6e86",
+    "url": "/HollywoodHandshakes/static/js/main.df2f455e.chunk.js"
+  },
+  {
+    "revision": "0b4d1247bc0d7ea1a001",
+    "url": "/HollywoodHandshakes/static/js/runtime-main.70861f19.js"
+  },
+  {
+    "revision": "514aa3b99f4f9a96851d8684c2c4208f",
+    "url": "/HollywoodHandshakes/static/media/Architect.514aa3b9.ttf"
+  },
+  {
+    "revision": "fb7810a162bf25d3929a3e9e0b8120cd",
+    "url": "/HollywoodHandshakes/static/media/Cassandra.fb7810a1.ttf"
+  }
+]);
