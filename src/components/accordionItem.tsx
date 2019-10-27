b576a4b06789df8376d6be0eeed7724f80b9aad9
Seb Zapata <sebzapata@hotmail.co.uk>
2019-10-27 18:02:36 +0000

    create accordion and renamed title

---
+++ src/components/accordionItem.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import plusIcon from "../images/miscellaneous/iconmonstr-plus-1.svg";
+import minusIcon from "../images/miscellaneous/iconmonstr-minus-1.svg";
+interface Props {
+  title: string;
+}
+
+interface State {
+  accordionOpen: boolean;
+}
+export class AccordionItem extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      accordionOpen: false
+    }
+  }
+
+  render() {
+    const styles = {
+      buttonStyle: {
+
+      }
+    };
+    return (
+      <div className="accordion__section">
+        <div className="accordion__section__header" onClick={() => this.setState({accordionOpen: !this.state.accordionOpen})}>
+          <button className="accordion__section__header__title">{this.props.title}</button>
+          <img className="accordion__section__header__icon" src={this.state.accordionOpen ? minusIcon : plusIcon}/>
+        </div>
+        {this.state.accordionOpen && <div className="accordion__section__panel">
+          {this.props.children}
+        </div>}
+      </div>
+    )
+  }
+}
