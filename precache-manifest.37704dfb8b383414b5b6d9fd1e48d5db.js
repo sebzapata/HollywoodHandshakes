70cc5a7a84216bf870a37eaf039af1bc65fe53e7
Sebastian Zapata <Sebastian.Zapata@bjss.com>
2019-10-29 12:08:21 +0000

    Updates

---
+++ precache-manifest.37704dfb8b383414b5b6d9fd1e48d5db.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41c6ff1738efc5cd18033ccfb96eefa",
+    "url": "/HollywoodHandshakes/index.html"
+  },
+  {
+    "revision": "2656bcbaba4549a474a6",
+    "url": "/HollywoodHandshakes/static/css/main.f36b5ff6.chunk.css"
+  },
+  {
+    "revision": "824ff42d5fbee0c8ac50",
+    "url": "/HollywoodHandshakes/static/js/2.6d0081ca.chunk.js"
+  },
+  {
+    "revision": "2656bcbaba4549a474a6",
+    "url": "/HollywoodHandshakes/static/js/main.256551ff.chunk.js"
+  },
+  {
+    "revision": "0b4d1247bc0d7ea1a001",
+    "url": "/HollywoodHandshakes/static/js/runtime-main.70861f19.js"
+  },
+  {
+    "revision": "514aa3b99f4f9a96851d8684c2c4208f",
+    "url": "/HollywoodHandshakes/static/media/Architect.514aa3b9.ttf"
+  },
+  {
+    "revision": "fb7810a162bf25d3929a3e9e0b8120cd",
+    "url": "/HollywoodHandshakes/static/media/Cassandra.fb7810a1.ttf"
+  },
+  {
+    "revision": "f89501621cd8869f90802eecdfd28793",
+    "url": "/HollywoodHandshakes/static/media/HussarBold.f8950162.otf"
+  },
+  {
+    "revision": "76f605eaabf51ffcc02130c128c5da32",
+    "url": "/HollywoodHandshakes/static/media/HussarBoldSuperCondensed.76f605ea.otf"
+  },
+  {
+    "revision": "3e6d933238bb3cccb993aec7b6e274c4",
+    "url": "/HollywoodHandshakes/static/media/S03E07 - Ryan.3e6d9332.png"
+  },
+  {
+    "revision": "a64c0d16f9706ee2cb01b885b1c919c5",
+    "url": "/HollywoodHandshakes/static/media/S04E09 - Frances.a64c0d16.png"
+  },
+  {
+    "revision": "5d12fdf2a25eab060c03af9b68ec568e",
+    "url": "/HollywoodHandshakes/static/media/S05E02 - Norman.5d12fdf2.png"
+  },
+  {
+    "revision": "8a081382ecbf4cb9671d13ad13591e6c",
+    "url": "/HollywoodHandshakes/static/media/S05E03 - Luis.8a081382.png"
+  },
+  {
+    "revision": "15542ca8a3ec8d353d97a7f98f6ba0e8",
+    "url": "/HollywoodHandshakes/static/media/S06E03 - Paul.15542ca8.png"
+  },
+  {
+    "revision": "4e6f0b29c7f1f05368bcf8993e95d8b9",
+    "url": "/HollywoodHandshakes/static/media/S06E07 - Tamal.4e6f0b29.png"
+  },
+  {
+    "revision": "ceadc9f264a76729d789e59ea19e773d",
+    "url": "/HollywoodHandshakes/static/media/S06E09 - Nadiya.ceadc9f2.png"
+  },
+  {
+    "revision": "eaf601ba46b174c72fc074b993e4a1e9",
+    "url": "/HollywoodHandshakes/static/media/S07E02 - Tom.eaf601ba.png"
+  },
+  {
+    "revision": "28572a0c424bcc86043748a73961cecc",
+    "url": "/HollywoodHandshakes/static/media/S07E04 - Selasi.28572a0c.png"
+  },
+  {
+    "revision": "7b80071e78edd831771c10c1078bddd8",
+    "url": "/HollywoodHandshakes/static/media/S07E10(1) - Candice.7b80071e.png"
+  },
+  {
+    "revision": "800c29f55c1c0a63f56d01acb61cf3b3",
+    "url": "/HollywoodHandshakes/static/media/S07E10(2) - Jane.800c29f5.png"
+  },
+  {
+    "revision": "7629227c552522de19dd36c291a2beec",
+    "url": "/HollywoodHandshakes/static/media/S08E01(1) - Steven.7629227c.png"
+  },
+  {
+    "revision": "1a99e97454487ff7700e57919488131c",
+    "url": "/HollywoodHandshakes/static/media/S08E01(2) - Sophie.1a99e974.png"
+  },
+  {
+    "revision": "a67e12508dc7bcbe02bbf73743bee0e5",
+    "url": "/HollywoodHandshakes/static/media/S08E04 - Liam.a67e1250.png"
+  },
+  {
+    "revision": "489d3665f111ebe71bfe83170985c49f",
+    "url": "/HollywoodHandshakes/static/media/S08E05 (1) - Steven.489d3665.png"
+  },
+  {
+    "revision": "783f02e8d85768a184376ae9ac092a1b",
+    "url": "/HollywoodHandshakes/static/media/S08E05 (2) - Yan.783f02e8.png"
+  },
+  {
+    "revision": "40835fe374c25be65946eb10088c028e",
+    "url": "/HollywoodHandshakes/static/media/S08E05 (3) - Stacey.40835fe3.png"
+  },
+  {
+    "revision": "f266585a9e2999804d0a2aeaad1257d5",
+    "url": "/HollywoodHandshakes/static/media/S08E08 - Stacey.f266585a.png"
+  },
+  {
+    "revision": "45090443bea2e1aa9384fcf4c157e2df",
+    "url": "/HollywoodHandshakes/static/media/S09E02 (1) - Dan.45090443.png"
+  },
+  {
+    "revision": "35bbc866c2ee4326f49bba2642b02138",
+    "url": "/HollywoodHandshakes/static/media/S09E02 (2) - Rahul.35bbc866.png"
+  },
+  {
+    "revision": "e2bd0011b167db1a5389b743732c8c15",
+    "url": "/HollywoodHandshakes/static/media/S09E02 (3) - Ruby.e2bd0011.png"
+  },
+  {
+    "revision": "60e6d83e2994708827f888cc1d596ce9",
+    "url": "/HollywoodHandshakes/static/media/S09E03 - Dan.60e6d83e.png"
+  },
+  {
+    "revision": "84e37ed53b31157bb47f443b938a7594",
+    "url": "/HollywoodHandshakes/static/media/S09E04 (1) - Dan.84e37ed5.png"
+  },
+  {
+    "revision": "1adbd6e33946363a3a4d55cc538b5559",
+    "url": "/HollywoodHandshakes/static/media/S09E04 (2) - Jon.1adbd6e3.png"
+  },
+  {
+    "revision": "1948b3390f4ea8234f0182ce3083047c",
+    "url": "/HollywoodHandshakes/static/media/S09E05 (1) - Rahul.1948b339.png"
+  },
+  {
+    "revision": "b2371fe66b3944a609e32841f1cb5422",
+    "url": "/HollywoodHandshakes/static/media/S09E05 (2) - Kim-Joy.b2371fe6.png"
+  },
+  {
+    "revision": "2c5f51d2a997270e692fbbfe8ddd94b5",
+    "url": "/HollywoodHandshakes/static/media/S09E05 (3) - Manon.2c5f51d2.png"
+  },
+  {
+    "revision": "b316691b3e61a9634a64c49d21efa571",
+    "url": "/HollywoodHandshakes/static/media/S09E06 (1) - Ruby.b316691b.png"
+  },
+  {
+    "revision": "d8bdafc032d0c19c4632a679c30ecb8e",
+    "url": "/HollywoodHandshakes/static/media/S09E06 (2) - Rahul.d8bdafc0.png"
+  },
+  {
+    "revision": "b8eacb25acc9fa1033cf6d1d1bbd89e1",
+    "url": "/HollywoodHandshakes/static/media/S09E07 (1) - Kim-Joy.b8eacb25.png"
+  },
+  {
+    "revision": "3280e9777bac4f0f5058b53e3f6b5595",
+    "url": "/HollywoodHandshakes/static/media/S10E03 - Michael.3280e977.png"
+  },
+  {
+    "revision": "1e7f88de259509cba11503e0b63c3668",
+    "url": "/HollywoodHandshakes/static/media/S10E05 - David.1e7f88de.png"
+  },
+  {
+    "revision": "41a96782e3259f6423a5d2f5f302e6d6",
+    "url": "/HollywoodHandshakes/static/media/S10E07 (1) - Steph.41a96782.png"
+  },
+  {
+    "revision": "a0cf181c34d6fa5a8904ee6146acfcff",
+    "url": "/HollywoodHandshakes/static/media/S10E07 (2) - Henry.a0cf181c.png"
+  },
+  {
+    "revision": "b04340ba59964ace13846da9ebf887ff",
+    "url": "/HollywoodHandshakes/static/media/iconmonstr-minus-1.b04340ba.svg"
+  },
+  {
+    "revision": "925bd082901ff89a3355b7c8075c1ac0",
+    "url": "/HollywoodHandshakes/static/media/iconmonstr-plus-1.925bd082.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HollywoodHandshakes/precache-manifest.4b7ac2742da40d374c78290e78d0507d.js"
+  "/HollywoodHandshakes/precache-manifest.37704dfb8b383414b5b6d9fd1e48d5db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HollywoodHandshakes/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
