9e7d08fc4d7dc47a3586974c93192ae499f4a65e
Sebastian Zapata <Sebastian.Zapata@bjss.com>
2019-11-04 14:31:13 +0000

    created modal component

--- src/components/photoWithDetails.tsx
+++ src/components/photoWithDetails.tsx
@@ -9,11 +9,68 @@ interface Props {
   season: Season
 }
 
-export const PhotoWithDetails = (props: Props) => {
-  return (
-    <div className="handshake">
-      <img className="handshake__image" src={props.handshake.image} alt={`Handshake being given to ${props.handshake.recipient}`} key={props.handshake.image}/>
-      <p className="handshake__description__text">{`Episode ${props.episode.number}, ${props.handshake.recipient}, ${props.handshake.challenge}`}</p>
-    </div>
-  )
-};
+interface State {
+  isOpen: boolean;
+}
+
+export class PhotoWithDetails extends React.Component<Props, State>{
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    }
+  }
+
+  componentDidMount(): void {
+    document.addEventListener("keydown", this.escapeKeyDown);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener("keydown", this.escapeKeyDown);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.renderHandshake(true)}
+        {this.renderModal(this.state.isOpen)}
+      </React.Fragment>
+    )
+  }
+
+  toggleImage = (event: React.MouseEvent<HTMLElement>, limitClickTo?: string) => {
+    if (!limitClickTo || ((event.target as HTMLElement).className === limitClickTo)) {
+      this.setState({
+        isOpen: !this.state.isOpen
+      })
+    }
+  };
+
+  renderHandshake = (clickable: boolean, className?: string) => {
+    return (
+      <div className={`handshake ${className}`} onClick={(e) => clickable && this.toggleImage(e)}>
+        <img className="handshake__image" src={this.props.handshake.image} alt={`Handshake being given to ${this.props.handshake.recipient}`} key={this.props.handshake.image}/>
+        <p className="handshake__description__text">{`Episode ${this.props.episode.number}, ${this.props.handshake.recipient}, ${this.props.handshake.challenge}`}</p>
+      </div>
+    )
+  };
+
+  renderModal = (isOpen: boolean) => {
+    if (!isOpen) return null;
+
+    return (
+      <div className="modal" onClick={(e) => this.toggleImage(e, "modal")}>
+        {this.renderHandshake(false, "handshake--large")}
+      </div>
+    )
+  };
+
+  escapeKeyDown = (event: KeyboardEvent ) => {
+    if (event.code === "Escape") {
+      this.setState({
+        isOpen: false
+      })
+    }
+  }
+}
