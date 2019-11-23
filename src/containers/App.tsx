ee615fa09df88f0c2d30d854fa93820a9401378f
Sebastian Zapata <Sebastian.Zapata@bjss.com>
2019-11-23 19:02:25 +0000

    adjust view height calculation

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -43,6 +43,11 @@ const App: React.FC = () => {
     }
   ];
 
+  window.addEventListener('resize', () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
   return (
     <div className="page">
       <Header/>
