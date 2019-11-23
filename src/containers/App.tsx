413e88f415ccf2a78d01ace6bc26feb1960fff90
Sebastian Zapata <Sebastian.Zapata@bjss.com>
2019-11-23 19:47:30 +0000

    turn app into class

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -8,9 +8,14 @@ import { PhotoWithDetails } from "../components/photoWithDetails";
 import { Data } from "../data/data";
 import { Season } from "../types/Season";
 
+class App extends React.Component {
 
-const App: React.FC = () => {
-  const getHandshakes = (season: Season) => {
+  componentDidMount(): void {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  getHandshakes = (season: Season) => {
     const handshakes = season.episodes.map(e => e.handshakes);
 
     if(handshakes.every(h => h === null)) {
@@ -24,41 +29,43 @@ const App: React.FC = () => {
     });
   };
 
-  const renderAccordions = () => {
+  renderAccordions = () => {
     return Data.map(x => (
       <AccordionItem title={`Season ${x.number} (${x.network})`} key={`Season${x.number}`}>
-        {getHandshakes(x)}
+        {this.getHandshakes(x)}
       </AccordionItem>
     ))
   };
 
-  const links = [
-    {
-      text: `© ${new Date().getFullYear()} hollywoodhandshakes.com`,
-      route: "https://www.hollywoodhandshakes.com"
-    },
-    {
-      text: "About",
-      route: "https://github.com/sebzapata/HollywoodHandshakes"
-    }
-  ];
+  render() {
+    window.addEventListener('resize', () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
 
-  window.addEventListener('resize', () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+    const links = [
+      {
+        text: `© ${new Date().getFullYear()} hollywoodhandshakes.com`,
+        route: "https://www.hollywoodhandshakes.com"
+      },
+      {
+        text: "About",
+        route: "https://github.com/sebzapata/HollywoodHandshakes"
+      }
+    ];
 
-  return (
-    <div className="page">
-      <Header/>
-      <div className="page__content">
-        <Accordion>
-          {renderAccordions()}
-        </Accordion>
+    return (
+      <div className="page">
+        <Header/>
+        <div className="page__content">
+          <Accordion>
+            {this.renderAccordions()}
+          </Accordion>
+        </div>
+        <Footer links={links}/>
       </div>
-      <Footer links={links}/>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
