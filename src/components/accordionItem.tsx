c4c1769a7302854f8f4a67fc1fda512b09414afa
Seb Zapata <sebzapata@hotmail.co.uk>
2020-10-05 12:33:36 +0100

    click tracking added for accordions and watermark style changed

--- src/components/accordionItem.tsx
+++ src/components/accordionItem.tsx
@@ -9,8 +9,9 @@ interface Props {
 export const AccordionItem: React.FunctionComponent<Props> = ({ title, children }) => {
   const [isAccordionOpen, setIsAccordionOpen] = useState(false);
 
+  const umamiId = title.match(/Season \d+/g)[0].replace(/\s/g, '');
   return (
-    <div className="accordion__section">
+    <div className={`accordion__section umami--click--${umamiId}Accordion`}>
       <div className="accordion__section__header" onClick={() => setIsAccordionOpen(!isAccordionOpen)}>
         <button className="accordion__section__header__title">{title}</button>
         <img className="accordion__section__header__icon" src={isAccordionOpen ? minusIcon : plusIcon} alt={isAccordionOpen ? "Close accordion" : "Open accordion"}/>
