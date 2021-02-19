be79ce1c7ada372d643654fad8bc1b7508dd334d
Seb Zapata <sebzapata@hotmail.co.uk>
2021-02-19 22:59:53 +0000

    update header and footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export const Header: React.FunctionComponent = () => (
   <div className="header">
-    <h1 className="header__title">Hollywood Handshakes</h1>
+    <h1 className="header__title">
+      <a href="/">Hollywood Handshakes</a>
+    </h1>
     <h2 className="header__subtitle">
       <span className="header__subtitle__span">The definitive collection <br className="header__subtitle__break" />of Paul Hollywood's infamous handshakes</span>
     </h2>
