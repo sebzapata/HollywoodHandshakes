703e70379db5b2ad0168c8a18a2e45102bc1296c
Seb Zapata <sebzapata@hotmail.co.uk>
2021-09-27 18:14:34 +0100

    change to serverless

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  target: "serverless",
   images: {
     // limit of 25 deviceSizes values
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
