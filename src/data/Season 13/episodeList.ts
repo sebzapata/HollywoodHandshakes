d03166044803d16194ac18db88eca6d4231be011
Sebastian Zapata <Sebastian.Zapata@davies-group.com>
2022-10-25 21:09:28 +0100

    season 13 ep 6

--- src/data/Season 13/episodeList.ts
+++ src/data/Season 13/episodeList.ts
@@ -3,6 +3,8 @@ import { Season13Contestants } from "../../types/constants/Season13Contestants";
 import { Episode } from "../../types/Episode";
 import Handshake45Large from "../../../public/handshakes/Large/S13E02 - Dawn.jpg";
 import Handshake46Large from "../../../public/handshakes/Large/S13E02 - Maxy.jpg";
+import Handshake47Large from "../../../public/handshakes/Large/S13E06 - Maxy.png";
+import Handshake48Large from "../../../public/handshakes/Large/S13E06 - Syabira.png";
 
 export const episodeList: Episode[] = [
   {
@@ -26,6 +28,27 @@ export const episodeList: Episode[] = [
         position: 1,
         image: {
           large: Handshake46Large,
+        },
+      },
+    ],
+  },
+  {
+    number: 6,
+    handshakes: [
+      {
+        recipient: Season13Contestants.Maxy,
+        challenge: Challenge.Signature,
+        position: 1,
+        image: {
+          large: Handshake47Large,
+        },
+      },
+      {
+        recipient: Season13Contestants.Syabira,
+        challenge: Challenge.Signature,
+        position: 2,
+        image: {
+          large: Handshake48Large,
         },
       },
     ],
